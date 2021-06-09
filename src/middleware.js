af5fbdd106fedae33b325192f2ab6cb9526d8f41
Petras Slekys <p.slekys@gmail.com>
2021-06-09 14:17:02 +0300

    user data validation as middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,27 @@
 const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+
 const { jwtSecretKey } = require('./config');
 
+const userSchema = Joi.object({
+  email: Joi.string().email().max(250).trim().lowercase(),
+  password: Joi.string().min(6).max(250),
+});
+
 module.exports = {
+  async isAuthDataCorrect(req, res, next) {
+    let userData;
+    try {
+      userData = await userSchema.validateAsync({
+        email: req.body.email,
+        password: req.body.password,
+      });
+      req.userData = userData;
+      return next();
+    } catch (e) {
+      return res.status(400).send({ error: 'Incorrect data passed' });
+    }
+  },
   isLoggedIn(req, res, next) {
     try {
       const token = req.headers.authorization?.split(' ')[1];
--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,37 +1,22 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcryptjs');
-const Joi = require('joi');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+const { isAuthDataCorrect } = require('../../middleware');
 const { mysqlConfig, jwtSecretKey } = require('../../config');
 
-const userSchema = Joi.object({
-  email: Joi.string().email().max(250).trim().lowercase(),
-  password: Joi.string().min(6).max(250),
-});
-
-router.post('/register', async (req, res) => {
-  let userData;
-  try {
-    userData = await userSchema.validateAsync({
-      email: req.body.email,
-      password: req.body.password,
-    });
-  } catch (e) {
-    return res.status(400).send({ error: 'Incorrect data passed' });
-  }
-
+router.post('/register', isAuthDataCorrect, async (req, res) => {
   try {
     const con = await mysql.createConnection(mysqlConfig);
 
-    const hashedPassword = bcrypt.hashSync(userData.password, 10);
+    const hashedPassword = bcrypt.hashSync(req.userData.password, 10);
 
     const [data] = await con.execute(
       `INSERT INTO users (email, password) VALUES (${mysql.escape(
-        userData.email,
+        req.userData.email,
       )}, '${hashedPassword}')`,
     );
 
@@ -52,29 +37,22 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
-  let userData;
-  try {
-    userData = await userSchema.validateAsync({
-      email: req.body.email,
-      password: req.body.password,
-    });
-  } catch (e) {
-    return res.status(400).send({ error: 'Incorrect data passed' });
-  }
-
+router.post('/login', isAuthDataCorrect, async (req, res) => {
   try {
     const con = await mysql.createConnection(mysqlConfig);
 
     const [data] = await con.execute(
-      `SELECT * FROM users WHERE email = ${mysql.escape(userData.email)}`,
+      `SELECT * FROM users WHERE email = ${mysql.escape(req.userData.email)}`,
     );
 
     if (data.length === 0) {
       return res.status(400).send({ error: 'Email or password is incorrect' });
     }
 
-    const validEmail = bcrypt.compareSync(userData.password, data[0].password);
+    const validEmail = bcrypt.compareSync(
+      req.userData.password,
+      data[0].password,
+    );
 
     if (!validEmail) {
       return res.status(400).send({ error: 'Email or password is incorrect' });
